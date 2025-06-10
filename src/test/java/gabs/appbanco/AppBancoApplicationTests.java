0b2373fed0b78e606b764174c206e712e4ca916e
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2025-06-10 12:26:30 -0500

    Start: defining structure
    First commit. Defining modules to make the hexagonal architecture

---
+++ src/test/java/gabs/appbanco/AppBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package gabs.appbanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
